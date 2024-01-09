3970ce557b86a60c99372aa5e157b14af6146e11
fer <fer.leon@usal.es>
2024-01-09 20:03:33 +0100

    Firefox headless funcionando

--- Scrappers/Utilidades.py
+++ Scrappers/Utilidades.py
@@ -12,13 +12,13 @@ from urllib.parse import urlparse, unquote
 import time
 
 options = Options()
-options.add_argument("--headless")
+# options.add_argument("--headless")
 options.add_argument("--disable-gpu")
 options.add_argument("--no-sandbox")
 # options.add_argument("enable-automation")
 options.add_argument("--disable-infobars")
 options.add_argument("--disable-dev-shm-usage")
-options.add_argument("window-size=1400,1500")
+# options.add_argument("window-size=1400,1500")
 options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537")
 
 def scrape_pccomponentes(url):
